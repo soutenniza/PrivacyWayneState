78b2652cb51d485ac1ef5e8128626d470d6f830f
Zachary Rzotkiewicz <zackrzot@gmail.com>
2015-03-17 19:29:57 -0400

    :o)

--- src/main/java/com/springapp/mvc/CreateUserController.java
+++ src/main/java/com/springapp/mvc/CreateUserController.java
@@ -1,17 +1,32 @@
 package com.springapp.mvc;
 
+import com.springapp.mvc.model.Person;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
 
 /**
  * Created by Zachary on 3/16/2015.
  */
 @Controller
 public class CreateUserController {
-    @RequestMapping(value = "/createuser")
+    @RequestMapping(value = "/createuser", method = RequestMethod.GET)
     public ModelAndView index() {
         ModelAndView mav = new ModelAndView("createuser");
         return mav;
+    }
+
+
+    @RequestMapping(value = "/submitcreateuser", method = RequestMethod.POST)
+    public String createUser(@ModelAttribute("SpringWeb")Person person,
+                             ModelMap model) {
+        model.addAttribute("name", person.getName());
+
+
+        return "result";
     }
 }
