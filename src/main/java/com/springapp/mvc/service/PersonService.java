bd4a7b5ba94a5338500f531a83d905a960c8dd2d
Zachary Rzotkiewicz <zackrzot@gmail.com>
2015-03-28 17:04:38 -0400

    Update PersonService.java

--- src/main/java/com/springapp/mvc/service/PersonService.java
+++ src/main/java/com/springapp/mvc/service/PersonService.java
@@ -6,7 +6,6 @@ import com.springapp.mvc.model.Person;
 import com.springapp.mvc.repository.AttributeRepository;
 import com.springapp.mvc.repository.GroupRepository;
 import com.springapp.mvc.repository.PersonRepository;
-import com.sun.tools.doclint.HtmlTag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.neo4j.support.Neo4jTemplate;
 import org.springframework.data.neo4j.template.Neo4jOperations;
