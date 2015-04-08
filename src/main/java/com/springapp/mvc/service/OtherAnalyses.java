85fad6fa1fa1af9aaa1335f5f83a65b03b62fa79
Nariman Ammar <nammar@wayne.edu>
2015-04-07 22:54:35 -0400

    added place holders for attribute exposure methods to other analyses class

--- src/main/java/com/springapp/mvc/service/OtherAnalyses.java
+++ src/main/java/com/springapp/mvc/service/OtherAnalyses.java
@@ -1,7 +1,32 @@
 package com.springapp.mvc.service;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.springapp.mvc.model.Person;
+
+import java.util.HashMap;
+
 /**
  * Created by narimanammar on 4/6/15.
  */
+@Service
+@Transactional
 public class OtherAnalyses {
+    HashMap<String,Double> generateDirectRelationAttributeExposureMap(Person p){
+
+        //TODO: get attributes from p
+        //TODO: for each attribute calculate the exposure
+
+        return null;
+    }
+
+    HashMap<String,Double> generateTransitiveRelationAttributeExposureMap(Person p){
+
+        //TODO: get attributes from p
+        //TODO: for each attribute calculate the exposure
+
+        return null;
+    }
+
+    
 }
