cee9cb9ab0d2879a763bb89483542c477d359283
Nariman Ammar <nammar@wayne.edu>
2015-04-07 22:41:54 -0400

    added more method place holders to the Group analysis service class

--- src/main/java/com/springapp/mvc/service/GroupAnalysisService.java
+++ src/main/java/com/springapp/mvc/service/GroupAnalysisService.java
@@ -15,6 +15,13 @@ import java.util.Collection;
 @Transactional
 public class GroupAnalysisService {
 
+    /**
+     * * gets common groups between a friend and each of the mutual friends between him
+     * and each friend in his friends list
+     * @param r
+     * @param g
+     * @return
+     */
     public int mutualgroups(Person r, Person g){
         Collection<Group> rootGroups = r.getGroups();
         ArrayList<Long> rootLong = new ArrayList<>();
@@ -36,6 +43,12 @@ public class GroupAnalysisService {
         return rootLong.size();
     }
 
+    /**
+     * gets common groups between a friend and each friend in his friends list
+     * @param r
+     * @param p
+     * @return
+     */
     public int commonGroups(Person r, Person p){
         Collection<Group> rootGroups = null; //TODO: get groups
         ArrayList<Long> rootLong = new ArrayList<>();
@@ -44,4 +57,35 @@ public class GroupAnalysisService {
         return rootLong.size();
     }
 
+
+    /**
+     * Group information that we cannot directly pull but can infer:
+     -overlaps and containment within groups based on common attributes between a person and
+     persons in his network (friends and friends of friends).
+     a. family members
+     b. high school friends
+     c. college friends
+     d. friends under same advisor
+     e.g. all people in a user's network that are at WSU,
+     user's that are engineering,
+     CS users, ACM group,
+     * @param p
+     * @return
+     */
+    public int detectCommunitities(Person p){
+        //TODO: Dishank: implement me
+    return 0;
+    }
+
+    /**
+     * Based on the above find commonalities in privacy settings then notify based on
+     * outliers.
+     * For all attributes in p's profile, if on average group members are conservative
+     * regarding attribute dk then whoever falls below this average is an outlier
+     * @return
+     */
+    public int detectPrivacyOutliers(){
+
+        return 0;
+    }
 }
