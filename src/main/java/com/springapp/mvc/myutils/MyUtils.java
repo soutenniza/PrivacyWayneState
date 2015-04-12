ed46cbc46ebf6c15fe3b2399b941a054c8d752fa
Nariman Ammar <nammar@wayne.edu>
2015-04-12 19:06:56 -0400

    added more metric place holders to other analysis for now
    1. added a utility package JavaMI..
    2. added to other analyses class
    3. will move place holders to where they belong shortly
    4. added MyUtils file in case we need to map types between netKit,
    weka, and our types (model)

---
+++ src/main/java/com/springapp/mvc/myutils/MyUtils.java
@@ -0,0 +1,8 @@
+package com.springapp.mvc.myutils;
+
+/**
+ * Created by narimanammar on 4/11/15.
+ */
+public class MyUtils {
+
+}
--- src/main/java/com/springapp/mvc/service/OtherAnalyses.java
+++ src/main/java/com/springapp/mvc/service/OtherAnalyses.java
@@ -1,9 +1,12 @@
 package com.springapp.mvc.service;
 
+import com.springapp.mvc.model.Attribute;
+import com.springapp.mvc.model.Group;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.springapp.mvc.model.Person;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
@@ -65,7 +68,86 @@ public class OtherAnalyses {
         //People-SCORElab: http://score.cs.wayne.edu/People.html
 
 
-    return null;
+        return null;
+    }
+
+    /**
+     * group based attribute prediction
+     * for an attribute dk in p's profile, predict the value of that attribute based on the distribution
+     * of that attribute in a group g to which he belongs
+     * 1. if p belongs to one group only
+     *    use RelationalClassifiers in NetKit. Pick your favourate classifier!
+     *    @see http://netkit-srl.sourceforge.net/current/docs/
+     *    netkit.classifiers.relational.ClassDistribRelNeighbor
+     *    Contact Sofus A. Macskassy (sofmac@gmail.com) for clarifications
+     * 2. if p belongs to more than one group (difffernet distibutions!!)
+     *    select  groups relevant to the classification task by taking the following group properties into account:
+     *    density, size, homogenity, percentage of public profiles in it,
+     *    smaller groups are more predictive than larger groups
+     *    homogenious groups are more predictive
+     *    dense groups are more predictive
+     */
+    HashMap<Attribute,Double> predictAttributeFromGroups(Person p,Group g, Attribute dk){
+        //consider dk as the class label
+        //You may need to map a neo4j Attribute to the corresponding netkit.graph.Attribute
+        //mew Attribute(java.lang.String name, Type type)
+
+
+        //More than one group case
+        //1.get the set of relevent groups
+        ArrayList<Group> relevantgroups=null;
+
+        //determine group homogenity (0 highest homogenity, 1 lowest homogenity)
+        //More homogenious groups are more predictive of an attribute
+        // e.g. to predict dk=location, a group with 90% members from the same country
+        //is your best predictor of the location class label
+        double homogenity = 0;
+        Double dkk[]= new Double[g.getMembers().size()];
+
+        //calculate entropy of the group g based on attribute dk
+        // homogenity = JavaMI.Entropy.calculateEntropy(dkk);
+        //if(homogenious)
+        //add to relevant groups
+
+        //do the same for size and desnity etc.
+
+
+        //2. traverse the relevent groups and extract their values for dk
+        Double dkkrelevant[] = new Double[relevantgroups.size()];
+        //train a classifier on dkkrelevant
+        // get the class label for p's dk based on the trained classifier
+        return null;
+    }
+
+    /**
+     * For each of the above classification models assess their accuracy with respedt to the original
+     * value of dk
+     * @return
+     */
+    double calculateModelAccuracy(Person p, Attribute dk){
+        //TODO: Dishank, Van based on the result of this method, if the accuracy is high, display a warning in both
+        //group an relatioship what if analyses
+        //WARNING: your membership in this groups predicts your attribute dk with high accuracy
+        //WARNING: your relationship network predicts your attribute dk with high accuracy
+        return 0;
+    }
+
+    /**
+     * relationship based attribute prediction
+     * for an attribute dk in p's profile, predict the value of that attribute based on the distribution
+     * of that attribute in the friends with which he has relations
+     * Based on RelationalClassifiers in NetKit. Pick your favourate classifier!
+     * @see http://netkit-srl.sourceforge.net/current/docs/
+     *  netkit.classifiers.relational.ClassDistribRelNeighbor
+     *  Contact Sofus A. Macskassy (sofmac@gmail.com) for clarifications
+     */
+    HashMap<Attribute,Double> predictAttributeFromRelations(Person p, Attribute dk){
+        //consider dk as the class label
+        //You may need to map a neo4j Attribute to the corresponding netkit.graph.Attribute
+        //mew Attribute(java.lang.String name, Type type)
+
+
+        return null;
     }
 
 
